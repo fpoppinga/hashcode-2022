3af0e00c8baf18e0e60c093966083c24aea2950b
Finn Poppinga <fpoppinga@cc.systems>
2022-02-12 20:55:01 +0100

    general setup

---
+++ main.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+from typing import Iterable, List, Set
+
+
+@dataclass
+class Client:
+    id: int
+    likes: Set[str]
+    dislikes: Set[str]
+
+
+@dataclass
+class Pizza:
+    ingredients: Set[str]
+
+    def to_string(self) -> str:
+        return f"{len(self.ingredients)} {' '.join(self.ingredients)}"
+
+
+def read_input(problem: str) -> Iterable[Client]:
+    with open(f"./input/{problem}.in.txt", "r") as f:
+        lines = f.readlines()
+
+    num_clients = int(lines[0])
+    id = 0
+    while id < num_clients:
+        yield Client(
+            id,
+            likes=set(lines[id * 2 + 1].split()[1:]),
+            dislikes=set(lines[id * 2 + 2].split()[1:]),
+        )
+        id = id + 1
+
+
+def write_output(problem: str, solution: Pizza):
+    with open(f"./output/{problem}.out.txt", "w") as f:
+        f.write(solution.to_string())
+
+
+def score(clients: Iterable[Client], pizza: Pizza) -> int:
+    score = 0
+    for client in clients:
+        if pizza.ingredients.issuperset(client.likes) and pizza.ingredients.isdisjoint(
+            client.dislikes
+        ):
+            score = score + 1
+    return score
+
+
+if __name__ == "__main__":
+    for problem in [
+        "a_an_example",
+        "b_basic",
+        "c_coarse",
+        "d_difficult",
+        "e_elaborate",
+    ]:
+        clients = list(read_input(problem))
+        solution_pizza = Pizza({"cheese", "basil", "peppers"})
+        print(f"{problem}: {score(clients, solution_pizza)}")
+        write_output(problem, solution_pizza)
